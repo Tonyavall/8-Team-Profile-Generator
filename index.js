56c7377d8b048435f0ec278d9db634a6f7d98f69
Tonyavall <tonyavallescas@gmail.com>
2022-05-25 18:21:59 -0700

    Added Nodejs questions

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const fs = require('fs')
 const questions = require('questions')
 const generateHtml = require('generateHtml')
 
-const promptUser = () => inquirer.prompt(questions)
+const promptUser = () => inquirer.prompt(questions.init)
 
 const init = () => {
     promptUser()
         .then(answers => {
-        const fileName = `${answers.fileName.split(' ').join('')}.html`
+        const fileName = `${answers.init.fileName.split(' ').join('')}.html`
         fs.writeFileSync(fileName, generateHtml(answers))
         })
         .then(() => console.log('Successfully generated the Html'))
