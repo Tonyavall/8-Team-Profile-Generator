9fdfcfefbe46060ec6a24e22e67abe97d83582f4
Tonyavall <tonyavallescas@gmail.com>
2022-05-27 18:16:15 -0700

    Fixed init functionality and updated questions array

--- index.js
+++ index.js
@@ -7,39 +7,47 @@ const Intern = require('./lib/Intern')
 const questions = require('./lib/questions')
 const generateHtml = require('./lib/generateHtml')
 
-const members = []
-
 const promptUser = () => inquirer.prompt(questions)
 
-const init = () => {
+const init = members => {
+    const staff = members ? members : []
+
     promptUser()
         .then(answers => {
-            if (!answers.role) return console.log('Canceled')
-
-            switch(answers.role) {
+            switch (answers.role) {
                 case 'Manager':
-                    const manager = new Manager(answers.manager_name, answers.manager_id, answers.manager_email, answers.manager_office)
-                    members.push(manager)
+                    const manager = new Manager(
+                        answers.manager_name,
+                        answers.manager_id,
+                        answers.manager_email,
+                        answers.manager_office
+                    )
+                    staff.push(manager)
                     break;
                 case 'Engineer':
-                    const engineer = new Engineer(answers.engineer_name, answers.engineer_id, answers.engineer_email, answers.engineer_github)
-                    members.push(engineer)
+                    const engineer = new Engineer(
+                        answers.engineer_name,
+                        answers.engineer_id,
+                        answers.engineer_email,
+                        answers.engineer_github
+                    )
+                    staff.push(engineer)
                     break;
                 case 'Intern':
-                    const intern = new Intern(answers.intern_name, answers.intern_id, answers.intern_email, answers.intern_school)
-                    members.push(intern)
+                    const intern = new Intern(
+                        answers.intern_name,
+                        answers.intern_id,
+                        answers.intern_email,
+                        answers.intern_school
+                    )
+                    staff.push(intern)
                     break;
             }
-            return answers.add_new ? init() : members
+
+            if (answers.add_new) return init(staff)
+            fs.writeFileSync('./index.html', generateHtml(staff))
         })
-        .then(() => {
-            if (!members.length) {
-                throw new Error('No members were added.') 
-            }
-            fs.writeFileSync('index.html', generateHtml(members))
-        })
-        .then(() => console.log('Successfully generated the Html'))
         .catch(err => console.error(err))
 };
 
-init();
+init()
